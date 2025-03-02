d9d4fdb0d27897f6003f251941369dfe884113bf
kirtikganesan <kirtikganesan@gmail.com>
2025-03-03 01:46:19 +0530

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
