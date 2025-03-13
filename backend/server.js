c6ba0963a4756ac9a7d8c174ffecc55cb95ebcab
Kirtik Ganesan <kirtikganesan@gmail.com>
2025-03-13 14:12:40 +0530

    Changed frontend url

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const path = require("path");
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
