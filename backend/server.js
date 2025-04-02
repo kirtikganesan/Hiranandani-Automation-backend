213841eee4c3e2563f13b01fb1ebd638de6931c6
kirtikganesan <kirtikganesan@gmail.com>
2025-04-03 00:58:33 +0530

    .env files changed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({ 
-  origin: 'http://localhost:5173'
+  origin: 'https://hiranandani-automation.vercel.app'
 }));
 app.use(express.json());
 
